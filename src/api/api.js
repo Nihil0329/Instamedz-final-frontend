1f04489c6876efe942744bba3aabc9bdf60a2ddf
Instamedz <111243837+Instamedz@users.noreply.github.com>
2023-03-28 14:07:45 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 const API=axios.create({
-    // baseURL:'https://instamedzbackend.onrender.com'
-    baseURL:'http://localhost:8000'
+    baseURL:'https://instamedzbackend.onrender.com'
+//     baseURL:'http://localhost:8000'
 })
 
 export const imgendpoint="https://ik.imagekit.io/instamedz/"
