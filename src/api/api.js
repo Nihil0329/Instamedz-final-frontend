134625f26d11d43623e952c7b0c993da88a3cb4e
DBcoppr <mdtarikhan007@gmail.com>
2023-11-23 19:12:11 +0530

    added sheetdb to store patient appoint in excel sheet

--- src/api/api.js
+++ src/api/api.js
@@ -1,99 +1,90 @@
-import axios from "axios"
-const API=axios.create({
-    baseURL:'https://instamedzbackend.onrender.com'
-//     baseURL:'http://localhost:8000'
-})
+import axios from "axios";
+const API = axios.create({
+  baseURL: "https://instamedzbackend.onrender.com",
+  //     baseURL:'http://localhost:8000'
+});
 
-export const imgendpoint="https://ik.imagekit.io/instamedz/"
+export const imgendpoint = "https://ik.imagekit.io/instamedz/";
 
 // auth api
 
-export const logIn = async(authData)=>{
-    try{
-        const result = await API.post("/auth/login",authData)
-        return result
-    }
-    catch(error){
-        return error.response
-    }
-}
-export const signUp = async(authData)=>{
-    try{
-        const result = await API.post("/auth/signup",authData)
-        console.log(result)
-        return result
-    }
-    catch(error){
-        return error.response
-    }
-}
-export const appointform = async(authData)=>{
-    try{
-        const result = await API.post("/auth/formsubmit",authData)
-        console.log(result)
-        return result
-    }
-    catch(error){
-        return {msg:"error in login"}
-    }
-}
-export const mydata = async(uid)=>{
-    try{
-        const result = await API.get(`/auth/myappoint/${uid}`)
-        return result
-    }
-    catch(error){
-        return {msg:"error in login"}
-    }
-}
-export const changepass =async(authData)=>{
-    try{
-        const result = await API.post("/auth/changepassword",authData)
-        return result
-    }
-    catch(error){
-        return error.response
-    }
-}
-export const checksignup =async(authData)=>{
-    try{
-        const result = await API.post("/auth/checksignup",authData)
-        return result
-    }
-    catch(error){
-        return error.response
-    }
-}
+export const logIn = async (authData) => {
+  try {
+    const result = await API.post("/auth/login", authData);
+    return result;
+  } catch (error) {
+    return error.response;
+  }
+};
+export const signUp = async (authData) => {
+  try {
+    const result = await API.post("/auth/signup", authData);
+    console.log(result);
+    return result;
+  } catch (error) {
+    return error.response;
+  }
+};
+export const appointform = async (authData) => {
+  try {
+    const result = await API.post("/auth/formsubmit", authData);
+    console.log(result);
+    return result;
+  } catch (error) {
+    return { msg: "error in login" };
+  }
+};
+export const mydata = async (uid) => {
+  try {
+    const result = await API.get(`/auth/myappoint/${uid}`);
+    return result;
+  } catch (error) {
+    return { msg: "error in login" };
+  }
+};
+export const changepass = async (authData) => {
+  try {
+    const result = await API.post("/auth/changepassword", authData);
+    return result;
+  } catch (error) {
+    return error.response;
+  }
+};
+export const checksignup = async (authData) => {
+  try {
+    const result = await API.post("/auth/checksignup", authData);
+    return result;
+  } catch (error) {
+    return error.response;
+  }
+};
 // content api
 
-export const AddContent = async(addData)=>{
-    try{
-        const result = await API.post("/content/add",addData)
-        return result
-    }
-    catch(error){
-        return {msg:"error in adding content"}
-    }
-}
+export const AddContent = async (addData) => {
+  try {
+    const result = await API.post("/content/add", addData);
+    return result;
+  } catch (error) {
+    return { msg: "error in adding content" };
+  }
+};
 
-export const viewContent=async(care)=>{
-    try {
-        const careData= await API.post("/content/viewall",{care})
-        return careData
-    } catch (error) {
-        console.log("error in view content")
-    }
-}
+export const viewContent = async (care) => {
+  try {
+    const careData = await API.post("/content/viewall", { care });
+    return careData;
+  } catch (error) {
+    console.log("error in view content");
+  }
+};
 
 // doctor api
 
-export const viewDoctor=async(care)=>{
-    try {
-        const docData= await API.get(`/doctor/view/${care}`)
-        return docData
-    } catch (error) {
-        console.log("error in view Doctor")
-    }
-}
-
-
+export const viewDoctor = async (care) => {
+  try {
+    const docData = await API.get(`/doctor/view/${care}`);
+    return docData;
+  } catch (error) {
+    console.log("error in view Doctor");
+  }
+};
--- src/pages/Apphifi/Hifiform.js
+++ src/pages/Apphifi/Hifiform.js
@@ -1,86 +1,109 @@
-import React from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-import { useState } from 'react'
-import Footer from '../../components/Footer/Footer'
-import { appointform } from '../../api/api'
-import Navbar from '../../components/Navbar/Navbar'
+import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useState } from "react";
+import Footer from "../../components/Footer/Footer";
+import { appointform } from "../../api/api";
+import Navbar from "../../components/Navbar/Navbar";
 
 // import emailjs from '@emailjs/browser'
 function Hifiform() {
-    
-   
-    const [cname,setCname]=useState("")
-    const [phone,setPhone]=useState("")
-    const [date,setDate]=useState("")
-    const [mode,setMode]=useState("")
-    
-    const navigate=useNavigate()
-    const {id}=useParams()
-    const uid = localStorage.getItem("id")
-   
-   
-    console.log("form",uid)
-    // const sendEmail = (e) => {
-    //     e.preventDefault();
-        // emailjs.sendForm('service_yyr6ist', 'template_24pgpej', e.target, 'W8MIgqXfgJt3n4Jiz').then((result) => {
-        //     window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior) 
-        //     alert("✅Message sent successfully")
-        // }, (error) => {
-        //     console.log(error.text);
-        //     alert("❌ Message not sent ❌")
-    
-        // });
-    //   };
+  const [cname, setCname] = useState("");
+  const [phone, setPhone] = useState("");
+  const [date, setDate] = useState("");
+  const [mode, setMode] = useState("");
 
-    const bookapp=async(e)=>{
-        e.preventDefault()
-        const resultresp=await appointform({
-                uid:uid,
-                date:date,
-                Mode:mode,
-                phone:phone,
-                Docid:id,
-                Status:"pending"
-        })
-        if(resultresp){
-                navigate("/appointsuccess")
-        }
-        console.log(resultresp)
-        setCname("")
-        setPhone("")
-     
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const uid = localStorage.getItem("id");
+
+  console.log("form", uid);
+  // const sendEmail = (e) => {
+  //     e.preventDefault();
+  // emailjs.sendForm('service_yyr6ist', 'template_24pgpej', e.target, 'W8MIgqXfgJt3n4Jiz').then((result) => {
+  //     window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior)
+  //     alert("✅Message sent successfully")
+  // }, (error) => {
+  //     console.log(error.text);
+  //     alert("❌ Message not sent ❌")
+
+  // });
+  //   };
+
+  const bookapp = async (e) => {
+    e.preventDefault();
+    const resultresp = await appointform({
+      uid: uid,
+      date: date,
+      Mode: mode,
+      phone: phone,
+      Docid: id,
+      Status: "pending",
+    });
+
+    await axios.post("https://sheetdb.io/api/v1/725wae185vaj2", {
+      data: [
+        {
+          Name: cname,
+          Date: date,
+          Contact: phone,
+          Mode: mode,
+          formfor: "doc-appoint",
+        },
+      ],
+    });
+    if (resultresp) {
+      navigate("/appointsuccess");
     }
+    console.log(resultresp);
+    setCname("");
+    setPhone("");
+  };
   return (
     <div class="hifi-appform">
-        <Navbar/>
-    <div class="cube-app-container">
+      <Navbar />
+      <div class="cube-app-container">
         <div class="c-app-title">
-            
-            <div class="c-app-t1">
-                <h2>Appointment Request Form</h2>
-                <p>Please fill in the details for your appointment</p>
-            </div>
+          <div class="c-app-t1">
+            <h2> Form</h2>
+            <p>Please fill in the details for your appointment</p>
+          </div>
         </div>
 
-        <form method='POST'>
-            <div className="ask-form-container">
-                <article>
-                    <label for="cname">
-                        <h4>Name</h4>
-                        <input type="text" id="cname" onChange={(e)=>setCname(e.target.value)}   value={cname} placeholder=""></input>
-                    </label>
-                    <label for="cphone">
-                        <h4>Phone</h4>
-                        <input type="tel" id="cphone" onChange={(e)=>setPhone(e.target.value)}   value={phone}  ></input>
-                    </label>
-                </article>
+        <form method="POST">
+          <div className="ask-form-container">
+            <article>
+              <label for="cname">
+                <h4>Name</h4>
+                <input
+                  type="text"
+                  id="cname"
+                  onChange={(e) => setCname(e.target.value)}
+                  value={cname}
+                  placeholder=""
+                ></input>
+              </label>
+              <label for="cphone">
+                <h4>Phone</h4>
+                <input
+                  type="tel"
+                  id="cphone"
+                  onChange={(e) => setPhone(e.target.value)}
+                  value={phone}
+                ></input>
+              </label>
+            </article>
 
-                <article>
-                    <label for="cdate">
-                        <h4>Date</h4>
-                        <input type="date" id="cdate" onChange={(e)=>setDate(e.target.value)}   value={date}></input>
-                    </label>
-                    {/* <label for="cities">
+            <article>
+              <label for="cdate">
+                <h4>Date</h4>
+                <input
+                  type="date"
+                  id="cdate"
+                  onChange={(e) => setDate(e.target.value)}
+                  value={date}
+                ></input>
+              </label>
+              {/* <label for="cities">
                         <h4>City</h4>
                         <select class="form-control" onChange={(e)=>setCity(e.target.value)}
                         >
@@ -89,24 +112,32 @@ function Hifiform() {
                             <option value="indore">Indore,MP</option>
                         </select>
                     </label> */}
-                    <label for="callmode">
-                        <h4>Mode</h4>
-                        <select class="form-control"  onChange={(e)=>setMode(e.target.value)}
-                        >
-                            <option value="notselected"> --- select mode --- </option>
-                            <option value="phone">Phone</option>
-                            <option value="video">video</option>
-                            <option value="chat">chat</option>
-                        </select>
-                    </label>
-                </article>
-            </div>
-            <button onClick={bookapp}  class="btn btn-primary book-appointment" id="callback">Book Appointment</button>
+              <label for="callmode">
+                <h4>Mode</h4>
+                <select
+                  class="form-control"
+                  onChange={(e) => setMode(e.target.value)}
+                >
+                  <option value="notselected"> --- select mode --- </option>
+                  <option value="phone">Phone</option>
+                  <option value="video">video</option>
+                  <option value="chat">chat</option>
+                </select>
+              </label>
+            </article>
+          </div>
+          <button
+            onClick={bookapp}
+            class="btn btn-primary book-appointment"
+            id="callback"
+          >
+            Book Appointment
+          </button>
         </form>
+      </div>
+      <Footer />
     </div>
-    <Footer/>
-</div>
-  )
+  );
 }
 
-export default Hifiform
+export default Hifiform;
